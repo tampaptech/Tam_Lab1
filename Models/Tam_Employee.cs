446a0492ae8cee565f2a70b19cb8d723beb91106
tam.vo <tam.vo@pap-tech.com>
2020-08-12 11:06:05 +0700

    add Employee model.

---
+++ Models/Tam_Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tam_Lab1.Models
+{
+    //Id, Username, FirstName, LastName, DateOfBirth, Gender
+    public class Tam_Employee
+    { 
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+       
+        public DateTime DateOfBirth { get; set; }
+    
+        public string FirstName { get; set; }
+     
+        public string LastName { get; set; }
+
+        public string Gender { get; set; }
+
+    }
+}
