2ed26b04443b8fe4831cd3ce2d194e2d985502b1
tam.vo <tam.vo@pap-tech.com>
2020-08-12 11:27:54 +0700

    add EmployeeContext class

---
+++ Data/Tam_EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tam_Lab1.Models;
+
+namespace Tam_Lab1.Data
+{
+    public class Tam_EmployeeContext:DbContext
+    {
+        public Tam_EmployeeContext(DbContextOptions<Tam_EmployeeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tam_Employee> Tam_Employee { get; set; }
+    }
+}
